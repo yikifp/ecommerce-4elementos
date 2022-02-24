b250741f1bf4d8cfc25c9f30d3a2b10d839549b6
unknown <martinsazzaroli@gmail.com>
2022-02-23 22:43:10 -0300

    Update imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
-import Header from './components/header';
-import Footer from './components/footer';
-import Main from './components/main';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
 function App() {
   return (
